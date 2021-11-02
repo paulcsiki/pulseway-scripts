a88dbea61ed91951dc2ef6a21340dcbafad7b3b3
Alexandru Paul Csiki <paul.csiki@pulseway.com>
2021-11-02 17:28:26 +0000

    set-system-dns: Support for IPv6

--- set-system-dns/set-system-dns.ps1
+++ set-system-dns/set-system-dns.ps1
@@ -6,7 +6,8 @@
     Version:        1.0
     Author:         Alexandru Paul Csiki
     Creation Date:  16/10/2021
-    Purpose/Change: First version
+    Change Date:    02/11/2021
+    Purpose/Change: Added support for IPv6
 
 .EXAMPLE
     Copy and paste the contents of this script into a Pulseway PowerShell Automation Script.
@@ -14,11 +15,13 @@
 #>
 
 # Variables to be updated
-$primaryDNSServer     = "8.8.8.8";
-$secondaryDNSServer   = "1.1.1.1";                     # optional
-$pulsewayInstanceName = "";                            # optional
-$pulsewayUsername     = "";                            # optional
-$pulsewayPassword     = "";                            # optional
+$primaryIPv4DNSServer     = "8.8.8.8";
+$secondaryIPv4DNSServer   = "1.1.1.1";                 # optional
+$primaryIPv6DNSServer     = "2001:4860:4860::8888";    # optional
+$secondaryIPv6DNSServer   = "2001:4860:4860::8844";    # optional
+$pulsewayInstanceName     = "";                        # optional
+$pulsewayUsername         = "";                        # optional
+$pulsewayPassword         = "";                        # optional
 
 $adapters = Get-NetIPConfiguration;
 if ($adapters.Count -eq 0) {
@@ -30,18 +33,78 @@ $shouldNotify = $false;
 for ($i = 0; $i -lt $adapters.Count; $i++) {
     $currentAdapter = $adapters[$i];
 
-    if ($currentAdapter.IPv4DefaultGateway -ne $null) {
+    if ($currentAdapter.IPv4DefaultGateway -ne $null -or $currentAdapter.IPv6DefaultGateway -ne $null) {
         Write-Output "Checking $($currentAdapter.InterfaceAlias) ($($currentAdapter.InterfaceDescription))...";
-        $updatePrimaryDNS = ($currentAdapter.DNSServer.Count -lt 1) -or ($currentAdapter.DNSServer[0].Address.ToString() -ne $primaryDNSServer);
-        $updateSecondaryDNS = ($secondaryDNSServer -ne "") -and (($currentAdapter.DNSServer.Count -lt 2) -or ($currentAdapter.DNSServer[1].Address.ToString() -ne $secondaryDNSServer));
+        $updateDNS = $false;
+        $primaryIPv6Found = $false;
+        $secondaryIPv6Found = $false;
+        $primaryIPv4Found = $false;
+        $secondaryIPv4Found = $false;
 
-        if ($updatePrimaryDNS -or $updateSecondaryDNS) {
-            Write-Output "Updating interface $($currentAdapter.InterfaceAlias) ($($currentAdapter.InterfaceDescription)) DNS servers...";
-            if ($secondaryDNSServer -ne "") {
-                Set-DnsClientServerAddress -InterfaceIndex $currentAdapter.InterfaceIndex -ServerAddresses ($primaryDNSServer, $secondaryDNSServer);
-            } else {
-                Set-DnsClientServerAddress -InterfaceIndex $currentAdapter.InterfaceIndex -ServerAddresses ($primaryDNSServer);
+		for ($j = 0; $j -lt $currentAdapter.DNSServer.Count; $j++) {
+			$currentDNSServer = $currentAdapter.DNSServer[$j];
+            for ($k = 0; $k -lt $currentDNSServer.ServerAddresses.Count; $k++) {
+                $currentServerIP = $currentDNSServer.ServerAddresses[$k];
+
+                if ($currentDNSServer.AddressFamily -eq 23) { # 23 = 'IPv6'
+                    if ($primaryIPv6Found) {
+                        if ($secondaryIPv6DNSServer -eq '' -or $secondaryIPv6DNSServer -ne $currentServerIP) {
+                            $updateDNS = $true;
+                            break;
+                        }
+
+                        $secondaryIPv6Found = $true;
+                    } elseif ($secondaryIPv6Found) {
+                        $updateDNS = $true;
+                        Write-Output "More than two IPv6 DNS Servers found.";
+                        break;
+                    } else {
+                        if ($primaryIPv6DNSServer -eq '') {
+                            continue;
+                        }
+
+                        if ($primaryIPv6DNSServer -ne $currentServerIP) {
+                            $updateDNS = $true;
+                            break;
+                        }
+                        $primaryIPv6Found = $true;
+                    }
+                } else {
+                    if ($primaryIPv4Found) {
+                        if ($secondaryIPv4DNSServer -eq '' -or $secondaryIPv4DNSServer -ne $currentServerIP) {
+                            $updateDNS = $true;
+                            break;
+                        }
+
+                        $secondaryIPv4Found = $true;
+                    } elseif ($secondaryIPv4Found) {
+                        $updateDNS = $true;
+                        Write-Output "More than two IPv4 DNS Servers found.";
+                        break;
+                    } else {
+                        if ($primaryIPv4DNSServer -ne $currentServerIP) {
+                            $updateDNS = $true;
+                            break;
+                        }
+                        $primaryIPv4Found = $true;
+                    }
+                }
             }
+		}
+		
+        if ($updateDNS -ne $true) {
+            if ($secondaryIPv4DNSServer -ne '' -and $secondaryIPv4Found -ne $true) {
+                $updateDNS = $true;
+            } elseif ($primaryIPv6DNSServer -ne '' -and $primaryIPv6DNSServer -ne $true) {
+                $updateDNS = $true;
+            } elseif ($secondaryIPv6DNSServer -ne '' -and $secondaryIPv6DNSServer -ne $true) {
+                $updateDNS = $true;
+            }
+        }
+
+        if ($updateDNS) {
+            Write-Output "Updating interface $($currentAdapter.InterfaceAlias) ($($currentAdapter.InterfaceDescription)) DNS servers...";
+            Set-DnsClientServerAddress -InterfaceIndex $currentAdapter.InterfaceIndex -ServerAddresses ($primaryIPv4DNSServer, $secondaryIPv4DNSServer, $primaryIPv6DNSServer, $secondaryIPv6DNSServer);
             $shouldNotify = $true;
             Write-Output "Interface $($currentAdapter.InterfaceAlias) ($($currentAdapter.InterfaceDescription)) DNS servers updated.";
         }
